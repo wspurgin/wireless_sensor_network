5cdfabdb4092c7ff2b9b3b4a0327173e287ddba4
Will Spurgin <will.spurgin@gmail.com>
2016-11-26 11:00:11 -0600

    Add degree members to point for use in algorithms

--- lib/point.h
+++ lib/point.h
@@ -7,7 +7,26 @@ struct point {
   double x;
   double y;
 
-  point(long unsigned int i, double j, double k):id(i),x(j),y(k){}
+  // Degree should be used in algorithms as the unmodifiable degree. The
+  // curr_degree should be used in algorithms as a modifible entry on the point
+  // struct. Be sure to call reset on all points to restore all temporaries
+  // before applying any algorithms agains the temporary variables.
+  long unsigned int degree;
+
+  long unsigned int curr_degree;
+
+  void reset() { curr_degree = degree; }
+
+  point(long unsigned int i, double j, double k):id(i),x(j),y(k),degree(-1){
+    this->reset();
+  }
+
+  // with degree as well
+  point(long unsigned int i, double j, double k, double d):id(i),x(j),y(k),degree(d){
+    this->reset();
+  }
+
+  bool operator==(const point& rhs) const { rhs.id == this->id; }
 };
 
 #endif
