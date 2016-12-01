9c12ad592ca35db75d2f88494fc7e4da70e27589
Will Spurgin <will.spurgin@gmail.com>
2016-12-01 16:08:13 -0600

    Add coloring info to point.h

--- lib/point.h
+++ lib/point.h
@@ -1,11 +1,11 @@
 #ifndef POINT_H
 #define POINT_H
 
-// TODO add reference pointer to place in degree lists
 struct point {
   long unsigned int id;
   double x;
   double y;
+  long unsigned int color;
 
   // Degree should be used in algorithms as the unmodifiable degree. The
   // curr_degree should be used in algorithms as a modifible entry on the point
@@ -17,12 +17,17 @@ struct point {
 
   void reset() { curr_degree = degree; }
 
-  point(long unsigned int i, double j, double k):id(i),x(j),y(k),degree(-1){
+  point(long unsigned int i, double j, double k):id(i),x(j),y(k),degree(-1),color(0){
     this->reset();
   }
 
   // with degree as well
-  point(long unsigned int i, double j, double k, double d):id(i),x(j),y(k),degree(d){
+  point(long unsigned int i, double j, double k, double d):id(i),x(j),y(k),degree(d),color(0){
+    this->reset();
+  }
+
+  // with degree and color
+  point(long unsigned int i, double j, double k, double d, long unsigned int c):id(i),x(j),y(k),degree(d),color(c){
     this->reset();
   }
 
