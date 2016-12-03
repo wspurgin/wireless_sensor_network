146361fd3e54e841c4cb2e2408f4d4604fe3eca0
Will Spurgin <will.spurgin@gmail.com>
2016-12-03 11:41:42 -0600

    Add backbone indicator to point data

--- lib/point.h
+++ lib/point.h
@@ -6,6 +6,7 @@ struct point {
   double x;
   double y;
   long unsigned int color;
+  long unsigned int backbone;
 
   // Degree should be used in algorithms as the unmodifiable degree. The
   // curr_degree should be used in algorithms as a modifible entry on the point
@@ -17,21 +18,28 @@ struct point {
 
   void reset() { curr_degree = degree; }
 
-  point(long unsigned int i, double j, double k):id(i),x(j),y(k),degree(-1),color(0){
+  point(long unsigned int i, double j, double k):id(i),x(j),y(k),degree(-1),color(0),backbone(0){
     this->reset();
   }
 
   // with degree as well
-  point(long unsigned int i, double j, double k, double d):id(i),x(j),y(k),degree(d),color(0){
+  point(long unsigned int i, double j, double k, double d):id(i),x(j),y(k),degree(d),color(0),backbone(0){
     this->reset();
   }
 
   // with degree and color
-  point(long unsigned int i, double j, double k, double d, long unsigned int c):id(i),x(j),y(k),degree(d),color(c){
+  point(long unsigned int i, double j, double k, double d, long unsigned int c):id(i),x(j),y(k),degree(d),color(c),backbone(0){
+    this->reset();
+  }
+
+  // with degree and color and backbone
+  point(long unsigned int i, double j, double k, double d, long unsigned int c, long unsigned int b):id(i),x(j),y(k),degree(d),color(c),backbone(b){
     this->reset();
   }
 
   bool operator==(const point& rhs) const { return rhs.id == this->id; }
+
+  bool hasBackbone() const { return this->backbone != 0; }
 };
 
 #endif
