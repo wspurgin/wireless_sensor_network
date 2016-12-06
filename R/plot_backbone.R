f5f58a20b7ac99751a18d4f6d6eb6856e079a8a2
Will Spurgin <will.spurgin@gmail.com>
2016-12-06 14:14:58 -0600

    Final report (include PDF)

--- R/plot_backbone.R
+++ R/plot_backbone.R
@@ -12,18 +12,24 @@ read_graph <- function(shape, nodes, average, dir="..") {
 }
 
 plot_graph <- function (rgg, adj) {
+  rgg <- rgg[order(rgg$id),]
   lim <- c(0,1)
   plot(rgg[,c("x","y")],
        col = ifelse(!is.na(rgg$degree) & rgg$degree == min(rgg$degree), "red", "black"),
        pch = ifelse(!is.na(rgg$degree) & rgg$degree == min(rgg$degree), 16, 1),
        ylim = lim,
        xlim = lim)
-  arrows(x0 = as.double(as.matrix(rgg[adj$p_id+1, "x"])), y0 = as.double(as.matrix(rgg[adj$p_id+1, "y"])),
-         x1 = as.double(as.matrix(rgg[adj$c_id+1, "x"])), y1 = as.double(as.matrix(rgg[adj$c_id+1, "y"])),
+  apply(adj, MARGIN = 1, FUN = function(edge) {
+  arrows(x0 = as.double(as.matrix(rgg[which(rgg$id == edge[1]), "x"])),
+         y0 = as.double(as.matrix(rgg[which(rgg$id == edge[1]), "y"])),
+         x1 = as.double(as.matrix(rgg[which(rgg$id == edge[2]), "x"])),
+         y1 = as.double(as.matrix(rgg[which(rgg$id == edge[2]), "y"])),
          length = 0)
+  })
 }
 
 cut_min <- function(rgg, adj) {
+  rgg <- rgg[order(rgg$id),]
   row_cut <- which(rgg$degree == min(rgg$degree))
   if (length(row_cut) > 1) {
     row_cut <- row_cut[1]
@@ -33,7 +39,7 @@ cut_min <- function(rgg, adj) {
 
   p_cut <- which(adj$p_id == node$id)
   c_cut <- which(adj$c_id == node$id)
-  adj <- adj[-p_cut | c_cut,]
+  adj <- adj[-c(p_cut, c_cut),]
 
   return(list(rgg=rgg, adj=adj))
 }
